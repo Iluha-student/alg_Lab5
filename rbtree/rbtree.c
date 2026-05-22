d85faa15b975ba7757d2969d852203dcb2027d69
Alicorn757 <131530695+Alicorn757@users.noreply.github.com>
2026-05-22 17:21:23 +0300

    Added createRBTree

--- rbtree/rbtree.c
+++ rbtree/rbtree.c
@@ -1,1 +1,39 @@
-// Полностью на ваше усмотрение (только переиспользуйте код из предыдущих лабораторных, если он вам подходит)
+#include "rbtree.h"
+
+RBTree* createRBTree(void) {
+    RBTree* rbtree = malloc(sizeof(RBTree));
+    if (!rbtree) return NULL;
+
+    rbtree->root = malloc(sizeof(RBNode));
+    if (!rbtree->root) return NULL;
+
+    rbtree->nil = malloc(sizeof(RBNode));
+    if (!rbtree->nil) return NULL;
+
+    rbtree->size = 1;
+
+    return rbtree;   
+}
+
+void freeRBTree(RBTree* tree) {
+    return;
+}
+
+void rbInsert(RBTree* tree, const char* key, int doc_id, const char* title) {
+    tree->size++;
+    PostingEntry post = {doc_id, title};
+    return;
+}
+
+Vector* rbSearch(const RBTree* tree, const char* key) {
+    return NULL;
+}
+
+void rbTraverse(
+    const RBTree* tree,
+    void (*visit)(const char* key, Vector* postings, void* ctx),
+    void* ctx
+)
+{
+    return;
+}
