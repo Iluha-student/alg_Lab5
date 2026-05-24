6e0e1764e6e1108f2945a12d3800492975b7d4fb
Чижов Егор <egor337889@gmail.com>
2026-05-24 13:51:21 +0300

    Update rbtree.c

--- rbtree/rbtree.c
+++ rbtree/rbtree.c
@@ -1,5 +1,6 @@
 // Полностью на ваше усмотрение (только переиспользуйте код из предыдущих лабораторных, если он вам подходит)
 // rbtree.c
+#define _POSIX_C_SOURCE 200809L
 #include "rbtree.h"
 #include <stdlib.h>
 #include <string.h>
